1fe26a3ac397664dbdd8026e84a982cc37c007c7
selvatamilan <123528025+selvatamilan@users.noreply.github.com>
2023-01-25 10:18:01 +0530

    Add files via upload

---
+++ LEAP YEAR.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int y;
+  printf("Enter the year :");
+  scanf("%d",&y);
+  if((y%400==0)||((y%4==0)&&(y%100!=0))){
+  	printf("Leap year");
+  }
+  else {
+  	printf("Not a leap year");
+  }
+  return 0;
+}
