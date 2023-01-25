e2979e6db788722f5cdff2b42eec8db6844004a8
selvatamilan <123528025+selvatamilan@users.noreply.github.com>
2023-01-25 11:15:52 +0530

    Add files via upload

---
+++ ODD OR EVEN.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int n;
+	printf("Enter the number :");
+	scanf("%d",&n);
+	if(n%2==0)
+	   {
+	   	printf("EVEN NUMBER");
+	   }
+	else
+	   {
+	   	printf("ODD NUMBER");
+	   }
+return 0;
+	   
+}
