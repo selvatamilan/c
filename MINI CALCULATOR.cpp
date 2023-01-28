86823cc5389bfcbc279d56300f0760fba3404d8a
selvatamilan <123528025+selvatamilan@users.noreply.github.com>
2023-01-28 14:46:59 +0530

    Add files via upload

---
+++ MINI CALCULATOR.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main() {
+    char operation;
+    int n1, n2;
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &operation);
+    printf("Enter two operands: ");
+    scanf("%d %d",&n1, &n2);
+
+    switch(operation)
+    {
+        case '+':
+            printf("%d + %d = %d",n1, n2, n1+n2);
+            break;
+
+        case '-':
+            printf("%d - %d = %d",n1, n2, n1-n2);
+            break;
+
+        case '*':
+            printf("%d * %d = %d",n1, n2, n1*n2);
+            break;
+
+        case '/':
+            printf("%d / %d = %d",n1, n2, n1/n2);
+            break;
+        default:
+            printf("Error! operator is not correct");
+    }
+
+    return 0;
+}
