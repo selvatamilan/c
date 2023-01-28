b8e62a04224016157eea36605aa5a5b065c83bf8
selvatamilan <123528025+selvatamilan@users.noreply.github.com>
2023-01-28 14:03:26 +0530

    Add files via upload

---
+++ VOWEL.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+	char ch;
+	printf("Enter the character :");
+	scanf("%c",&ch);
+	switch(ch){
+	   case('a'):{
+	   	printf("VOWELLLL!!!!!!!!");
+		break;
+	      }
+	   case('e'):{
+	   	printf("VOWELLLL!!!!!!!!");
+		break;
+	   }
+	   case('i'):{
+	   	printf("VOWELLLL!!!!!!!!");
+		break;
+	   }
+	   case('o'):{
+	   	printf("VOWELLLL!!!!!!!!");
+		break;
+	   }
+	   case('u'):{
+	   	printf("VOWELLLL!!!!!!!!");
+		break;
+	   }
+	   default:{
+	   	printf("NOT AN VOWELLL!!!!!!!");
+		break;
+	   }
+       }  
+	return 0;
+}
